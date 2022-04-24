d9b969ae2b3ad58ad39a89f64524fdc1adecfdcf
yosuke.imai.00 <yosuke.imai.00@gmail.com>
2022-04-24 19:32:50 +0900

    up

--- app/src/main/java/com/example/soundfrequencyrecorder/MainActivity.kt
+++ app/src/main/java/com/example/soundfrequencyrecorder/MainActivity.kt
@@ -11,12 +11,17 @@ import android.media.MediaRecorder
 import kotlin.concurrent.thread
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainActivity : AppCompatActivity() {
 
     private val SAMPLING_RATE:Int=44100
 
     private var bufSize: Int = 0
+
+    private  val FFT_SIZE: Int=4096
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         //Audio recordの生成
-        var audioRec = AudioRecord(MediaRecorder.AudioSource.MIC,
+        var audioRec: AudioRecord = AudioRecord(MediaRecorder.AudioSource.MIC,
             SAMPLING_RATE,AudioFormat.CHANNEL_IN_MONO,
             AudioFormat.ENCODING_PCM_16BIT,bufSize * 2)
 
@@ -47,12 +52,24 @@ class MainActivity : AppCompatActivity() {
     private fun run(audioRec:AudioRecord){
         var buf:ByteArray = ByteArray(bufSize*2)
 
+
         //レコーディング中
         while(audioRec.recordingState==AudioRecord.RECORDSTATE_RECORDING){
             //録音データ読み込み
             audioRec.read(buf,0,buf.size)
 
             //エンディアン変換
+            var bf:ByteBuffer = ByteBuffer.wrap(buf)
+            bf.order(ByteOrder.LITTLE_ENDIAN)
+
+            val si:Int=bf.position()
+            val ei:Int=bf.capacity()/2
+
+            val s= ShortArray(ei-si+1)
+
+            for(i in si..ei){
+                s[i]=bf.getShort(i)
+            }
         }
 
         //録音停止
